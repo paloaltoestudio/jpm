afb72b5b517ef00a7f239cd6768d5fe7f4714dd0
David <usmagrafico@gmail.com>
2022-04-11 18:16:15 -0500

    Add all chrimes

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,6 @@
 async function fetchChcrime() {
     try {
-        const response = await fetch('http://127.0.0.1:5500/info.json');
+        const response = await fetch('/info.json');
         const chrime = await response.json();
         return chrime;
     } catch (error) {
